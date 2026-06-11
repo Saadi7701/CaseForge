70d06cba77cd11a682dcde769ed076777b23529b
Saadi7701 <mrsami1330@gmail.com>
2026-06-11 22:01:23 +0500

    Fix Admin Seeding by disabling RequireNonAlphanumeric

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
     options.Password.RequiredLength = 6;
 })
 .AddEntityFrameworkStores<ApplicationDbContext>()
